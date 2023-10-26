d9bd8ca4217b2b12ec378e0e1a0a72b45c34d4b1
Seth R <sar1074@unh.edu>
2023-10-26 17:47:51 -0400

    abstracted create board functionality into Board.java class

--- server/src/main/java/edu/unh/cs/cs619/bulletzone/repository/InMemoryGameRepository.java
+++ server/src/main/java/edu/unh/cs/cs619/bulletzone/repository/InMemoryGameRepository.java
@@ -143,47 +143,8 @@ public class InMemoryGameRepository implements GameRepository {
     }
 
     public void create() {
-        if (game != null) {
-            return;
-        }
-        synchronized (this.monitor) {
-            this.game = new Game();
-
-            createFieldHolderGrid(game);
-            FieldEntities f = new FieldEntities();
-            game = f.set(game);
-
-        }
-    }
-
-    private void createFieldHolderGrid(Game game) {
-        synchronized (this.monitor) {
-            game.getHolderGrid().clear();
-            for (int i = 0; i < FIELD_DIM * FIELD_DIM; i++) {
-                game.getHolderGrid().add(new FieldHolder());
-            }
-
-            FieldHolder targetHolder;
-            FieldHolder rightHolder;
-            FieldHolder downHolder;
-
-            // Build connections
-            for (int i = 0; i < FIELD_DIM; i++) {
-                for (int j = 0; j < FIELD_DIM; j++) {
-                    targetHolder = game.getHolderGrid().get(i * FIELD_DIM + j);
-                    rightHolder = game.getHolderGrid().get(i * FIELD_DIM
-                            + ((j + 1) % FIELD_DIM));
-                    downHolder = game.getHolderGrid().get(((i + 1) % FIELD_DIM)
-                            * FIELD_DIM + j);
-
-                    targetHolder.addNeighbor(Direction.Right, rightHolder);
-                    rightHolder.addNeighbor(Direction.Left, targetHolder);
-
-                    targetHolder.addNeighbor(Direction.Down, downHolder);
-                    downHolder.addNeighbor(Direction.Up, targetHolder);
-                }
-            }
-        }
+        Board brd = new Board(this.game, this.monitor);
+        this.game = brd.getGame();
     }
 
 }
