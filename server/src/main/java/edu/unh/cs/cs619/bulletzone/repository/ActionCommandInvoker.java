868ddf6f99f312f26f927829696acf68c8c20783
Seth R <sar1074@unh.edu>
2023-10-27 13:13:11 -0400

    created inner class to hold more comprehensive info about history events

--- server/src/main/java/edu/unh/cs/cs619/bulletzone/repository/ActionCommandInvoker.java
+++ server/src/main/java/edu/unh/cs/cs619/bulletzone/repository/ActionCommandInvoker.java
@@ -12,11 +12,36 @@ import edu.unh.cs.cs619.bulletzone.model.TankDoesNotExistException;
 import jdk.internal.net.http.common.Pair;
 
 public class ActionCommandInvoker {
-    private Stack<GridEvent> history = new Stack<>();
+    public class hist_node {
+        Timestamp ts;
+        long tankId;
+        Command command;
+
+        public hist_node(Timestamp ts, long tankId, Command command) {
+            this.ts = ts;
+            this.tankId = tankId;
+            this.command = command;
+        }
+
+        public Command getCommand() {
+            return command;
+        }
+
+        public long getTankId() {
+            return tankId;
+        }
+
+        public Timestamp getTs() {
+            return ts;
+        }
+    }
+    private Stack<hist_node> history = new Stack<>();
     public boolean executeCommand(long tankId, Command command) throws IllegalTransitionException, LimitExceededException, TankDoesNotExistException {
         Boolean res = command.execute();
         if (res) {
-            history.push(new GridEvent(tankId + " - " + command.getCommandType(), new Timestamp(System.currentTimeMillis())));
+            //history.push(new GridEvent(tankId + " - " + command.getCommandType(), new Timestamp(System.currentTimeMillis())));
+            hist_node hist_event = new hist_node(new Timestamp(System.currentTimeMillis()), tankId, command);
+            history.push(hist_event);
         }
        // System.out.println("Added " + command.getCommandType() + " command!");
         return res;
